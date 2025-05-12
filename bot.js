71fe02a279e4c4a4d7c4acdb8d33f24f96f5c877
Daniel Rodrigues <danrod278@gmail.com>
2025-05-11 22:55:39 -0300

    salvando lista de emsnsagens

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 class Conversa{
-    constructor(telefone, primeiramensagem){
+    constructor(telefone, mensagem){
         this.telefone = telefone
-        this.primeiramensagem = primeiramensagem
+        this.mensagens = [mensagem]
     }
 
     contarTempo(){
@@ -10,10 +10,13 @@ class Conversa{
 
     juntarMensagens(mensagem){
         
+        try{this.mensagens.push(mensagem)}
+        catch(error){console.error(error)}
+        
     }
 
     enviarParaServidor(){
-
+        //envia para o servidor e sai de emEspera
     }
 
     salvarNoBD(mensagem){
@@ -22,40 +25,65 @@ class Conversa{
 }
 
 let mensagens = [
-    {"11949838238": {texto:"Oi", data:"_formato_time_stamp"}} // ultimas mensagens
+    {"11949838238": {texto:"Oi", data:"_formato_time_stamp"}},
+    {"119498385238": {texto:"Oi", data:"_formato_time_stam"}},
+    {"11949838238": {texto:"tudo bem?", data:"_formato_time_stamp"}},
+    {"11949838238": {texto:"Gostaria de trabalhar para vocÊs", data:"_ormato_time_stamp"}}, // ultimas mensagens
 ]
 let emEspera = [
-    "1193233433","11932323434", "1192334323" // se o telefone esta aqui é porque o objeto desse telefone ja foi criado
+     // se o telefone esta aqui é porque o objeto desse telefone ja foi criado. é necessário para que um contato não tenha multiplas respostas
 ]
 let objetos = []
 
-while (true){
+let aResponder =[]
+
+//while (true){}
     mensagens.forEach(mensagem=>{
-        const telefone = Object.keys(mensagem[0][0]) // pega o numero de telefone da mensagem
+        const telefone = Object.keys(mensagem)[0] // pega o numero de telefone da mensagem
+        
         var existe=false
         for (i=0;i<emEspera.length;i++){
             if(emEspera[i]==telefone){ // verifica se o objeto ja foi criado
                 existe=true
                 //adicionar mais uma mensagem ao objeto
                 
-                mensagens=apagaMensagemDoArray(mensagens, numero, data)//apaga a mensagem do array para não ser lida mais de uma vez
+
+                objetos.forEach(objeto=>{
+                    
+                    try {
+                        if(objeto.telefone==telefone){
+                            objeto.juntarMensagens(mensagem[telefone].texto)
+                            
+                            
+                        }   
+                    } catch (error) {
+                        console.error("Essa mensagem não existe", error)
+                    }
+                })
+                mensagens=apagaMensagemDoArray(mensagens, telefone, mensagem.data)//apaga a mensagem do array para não ser lida mais de uma vez
+                
+                
                 break
             }
         }
-        if(existe=false){
-            objetos.push(new Conversa(mensagem[telefone], mensagem[telefone].texto))
-            mensagens=apagaMensagemDoArray(mensagens, numero, data)//apaga a mensagem do array para não ser lida mais de uma vez
-            console.log(objetos);
+        if(existe==false){
+            objetos.push(new Conversa(telefone, mensagem[telefone].texto))
+            mensagens=apagaMensagemDoArray(mensagens, telefone)//apaga a mensagem do array para não ser lida mais de uma vez
+            emEspera.push(telefone)
         }
         
-    })
-}
+        
+})
+    
+console.log(objetos)    
+console.log(emEspera);
+console.log(mensagens)    
 
 
 function apagaMensagemDoArray(mensagens, numero, data){
     let nMensagens = mensagens.filter(obj=>{
         const mensagem = obj[numero]
-        return mensagem !== data 
+        return !(mensagem && mensagem.data !== data) 
     })
     return nMensagens
 }
